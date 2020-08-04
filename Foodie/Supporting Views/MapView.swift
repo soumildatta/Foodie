7263911822d76ab1c1a55b2b52312067752f0675
Soumil Datta <soumildatta@gmail.com>
2020-08-04 13:24:40 -0500

    Create Map View

---
+++ Foodie/Supporting Views/MapView.swift
@@ -0,0 +1,30 @@
+//
+//  MapView.swift
+//  Foodie
+//
+//  Created by Soumil Datta on 8/4/20.
+//  Copyright © 2020 Soumil Datta. All rights reserved.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: UIViewRepresentable {
+    func makeUIView(context: Context) -> MKMapView {
+        MKMapView(frame: .zero)
+    }
+
+    func updateUIView(_ uiView: MKMapView, context: Context) {
+        let coordinate = CLLocationCoordinate2D(
+            latitude: 22.5, longitude: 88.3)
+        let span = MKCoordinateSpan(latitudeDelta: 1.0, longitudeDelta: 1.0)
+        let region = MKCoordinateRegion(center: coordinate, span: span)
+        uiView.setRegion(region, animated: true)
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
