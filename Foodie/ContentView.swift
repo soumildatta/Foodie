eb8891db192cdcc5fcae2826927b712d364f9317
Soumil Datta <soumildatta@gmail.com>
2020-08-03 22:40:27 -0500

    Initial Commit

---
+++ Foodie/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Foodie
+//
+//  Created by Soumil Datta on 8/3/20.
+//  Copyright © 2020 Soumil Datta. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
