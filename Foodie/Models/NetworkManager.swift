d49b9e4e170ef9d900763a66b0d973440371e966
Soumil Datta <soumildatta@gmail.com>
2020-08-07 00:39:29 -0500

    Retrieve restaurant data based on the entity id from location api

--- Foodie/Models/NetworkManager.swift
+++ Foodie/Models/NetworkManager.swift
@@ -12,7 +12,8 @@ class NetworkManager: ObservableObject {
     var location = [Location]()
     @Published var restaurants = [RestaurantOuter]()
     
-    func fetchLocationData() {
+    func fetchRestaurantData() {
+        // first, get entity id from zomato location api
         if let url = URL(string: "https://developers.zomato.com/api/v2.1/locations?apikey=\(Keys.apiKey)&query=Oxford%2C%20Mississippi") {
             URLSession.shared.dataTask(with: url) { (data, _, error) in
                 if error == nil {
@@ -20,21 +21,20 @@ class NetworkManager: ObservableObject {
                         let results = try! JSONDecoder().decode(LocationResults.self, from: safeData)
                         DispatchQueue.main.async {
                             self.location = results.location_suggestions
-                        }
-                    }
-                }
-            }.resume()
-        }
-    }
-    
-    func fetchRestaurantData() {
-        if let url = URL(string: "https://developers.zomato.com/api/v2.1/location_details?apikey=\(Keys.apiKey)&entity_id=6922&entity_type=city") {
-            URLSession.shared.dataTask(with: url) { (data, _, error) in
-                if error == nil {
-                    if let safeData = data {
-                        let results = try! JSONDecoder().decode(RestaurantResults.self, from: safeData)
-                        DispatchQueue.main.async {
-                            self.restaurants = results.best_rated_restaurant
+                            
+                            // get restaurant data after getting entity id and type from zomato location api
+                            if let restaurantUrl = URL(string: "https://developers.zomato.com/api/v2.1/location_details?apikey=\(Keys.apiKey)&entity_id=\(self.location[0].entity_id)&entity_type=\(self.location[0].entity_type)") {
+                                URLSession.shared.dataTask(with: restaurantUrl) { (data, _, error) in
+                                    if error == nil {
+                                        if let safeData = data {
+                                            let results = try! JSONDecoder().decode(RestaurantResults.self, from: safeData)
+                                            DispatchQueue.main.async {
+                                                self.restaurants = results.best_rated_restaurant
+                                            }
+                                        }
+                                    }
+                                }.resume()
+                            }
                             
                         }
                     }
