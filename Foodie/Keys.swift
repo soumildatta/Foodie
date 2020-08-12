e13c086109c69171dd6f2c489c056719e575e0a2
Soumil Datta <35431692+soumildatta@users.noreply.github.com>
2020-08-12 00:10:36 -0500

    Add files via upload

---
+++ Foodie/Keys.swift
@@ -0,0 +1,13 @@
+//
+//  Keys.swift
+//  Foodie
+//
+//  Created by Soumil Datta on 8/6/20.
+//  Copyright © 2020 Soumil Datta. All rights reserved.
+//
+
+import Foundation
+
+struct Keys {
+    static let apiKey = "Enter API Key Here"
+}
